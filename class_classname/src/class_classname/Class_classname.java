b4636d3112d1c43b8502975d2bc7da5fe7b7367d
gyugyukyle <99346644+gyugyukyle@users.noreply.github.com>
2024-10-29 18:43:00 +0800

    Add files via upload

---
+++ class_classname/src/class_classname/Class_classname.java
@@ -0,0 +1,53 @@
+package class_classname;
+public class Class_classname {
+    public static void main(String[] args) {
+
+       /* class Box{
+            double width;double height;double depth;
+        }
+//box do not contain any methods yet
+//a class defines a new data type of data. In these case, the new data type is called box
+
+Box mybox=new Box();
+
+// to actually create a box object, you will the statement above
+
+    }
+
+
+*/
+
+
+class Box{ 
+        double width; double height;double depth;
+    }
+class BoxDemo {
+    public static void main(String[]args){
+        Box mybox= new Box();
+        double vol;
+    }
+}
+        
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
